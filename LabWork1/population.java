49f133024c91d7ab63fc304b8a04beae7632c5d9
Pooja17Yadav <103186257+Pooja17Yadav@users.noreply.github.com>
2022-04-22 12:52:45 +0530

    Add files via upload

---
+++ LabWork1/population.java
@@ -0,0 +1,12 @@
+package Assignment4question2;
+
+public class population {
+			
+	public static void main(String[] args) {
+	
+		double population1=175000;
+		double population2=262500;
+		double average_percent_population_per_year=(((population2-population1)/population1)*100)/10;
+		System.out.println("The average percent increase of population per year "+average_percent_population_per_year );
+	}
+}
