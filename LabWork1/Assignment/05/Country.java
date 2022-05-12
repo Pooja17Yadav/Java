aaef734429e0f64bf364dbb73cfde25e89cd2864
Pooja17Yadav <103186257+Pooja17Yadav@users.noreply.github.com>
2022-05-12 11:12:34 +0530

    Add files via upload

---
+++ LabWork1/Assignment/05/Country.java
@@ -0,0 +1,8 @@
+
+public class Country {
+
+	public void state()
+	{
+	System.out.println("Maharashtra is a state");
+	}
+}
---
+++ LabWork1/Assignment/05/Mumbai.java
@@ -0,0 +1,8 @@
+
+public class Mumbai extends Country {
+
+	public void state()
+	{
+		System.out.println("Mumbai is the capital city of the Maharashtra.");
+	}
+}
